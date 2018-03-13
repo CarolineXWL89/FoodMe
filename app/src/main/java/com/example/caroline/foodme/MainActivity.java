0ad019be5e9052b1ab5c0332683c951b8e7574a4
CarolineXWL89 <carolinexwliu89@hotmail.com>
2018-03-13 13:15:47 -0700

    #1 - project set-up

---
+++ app/src/main/java/com/example/caroline/foodme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.caroline.foodme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
