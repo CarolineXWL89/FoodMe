c27a293bdf5ebc07e9c8ab915e5fed86bf77f11a
maylisw <Maylisw31@gmail.com>
2018-03-15 12:45:59 -0700

    Maylis #1 - Created README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # FoodMe
+###Classes
+- Login
+- Create Account
+- EVERY OTHER THING
+    - Settings (Recycler View)
+        - account settings
+        - language
+        - notifications
+        - social media (STRETCH)
+    - Tools (botton nav)
+        - Your tools
+        - Add tools
+        - Recomended (nah)
+    - Home (bottom nav)
+        - Search
+        - Create
+        - favorites
+    
--- app/src/main/java/com/example/caroline/foodme/FoodApp.java
+++ app/src/main/java/com/example/caroline/foodme/FoodApp.java
@@ -3,11 +3,14 @@ package com.example.caroline.foodme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FoodApp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //todo create botton nav bar
+        //todo create nav menu
+        //todo create fragments and make calls
     }
 }
