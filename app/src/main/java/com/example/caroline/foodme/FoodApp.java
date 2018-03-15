2b86c1bf1159044d6165a5551c7cd9ea7b686413
nicPorcu <nicolo.porcu@gmail.com>
2018-03-15 14:02:48 -0700

    isn't this a great commit message

--- app/src/main/java/com/example/caroline/foodme/FoodApp.java
+++ app/src/main/java/com/example/caroline/foodme/FoodApp.java
@@ -1,5 +1,6 @@
 package com.example.caroline.foodme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,8 @@ public class FoodApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i= new Intent(this, HomePageActivity.class);
+        startActivity(i);
         //todo create botton nav bar
         //todo create nav menu
         //todo create fragments and make calls
---
+++ app/src/main/java/com/example/caroline/foodme/HomePageActivity.java
@@ -0,0 +1,50 @@
+package com.example.caroline.foodme;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_favorites);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_create);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+
+
+}
