bc87d5d1510efcf152b01b349174c5937d5c36ad
maylisw <Maylisw31@gmail.com>
2018-03-21 13:12:20 -0700

    Maylis #6 - Finished shared preferences michael needs to impelement

--- app/src/main/java/com/example/caroline/foodme/HomePageActivity.java
+++ app/src/main/java/com/example/caroline/foodme/HomePageActivity.java
@@ -1,15 +1,23 @@
 package com.example.caroline.foodme;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 /*
@@ -20,29 +28,31 @@ Can: be accessed by clicking on logo/home, NOT launching activity!!! (Need to ch
  */
 public class HomePageActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    View decorView;
+    /*
+    GET USERID //todo delete user exists when you log out
+    SharedPreferences sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        String userID = sharedPref.getString(getString(R.string.user_ID), "");*/
+    public static final String TAG = "YADA";
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        logIn(); //checks if user ahs already logged in, if not swtiches to log in screen
         wireWidgets();
-
-        hideNavBar();
     }
 
-    @Override
-    protected void onResume() {
-        hideNavBar();
-        super.onResume();
-    }
-
-    public void hideNavBar(){
-        decorView=getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOptions);
-
+    private void logIn() {
+        sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        Boolean userExists = sharedPref.getBoolean(getString(R.string.user), false); //checks if previous user exists
+        if(!userExists){ //if no user sends you to login
+            Intent i = new Intent(this, LoginScreen.class);
+            startActivity(i);
+        }
     }
 
     @Override
@@ -57,7 +67,6 @@ public class HomePageActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.settings_toolbar);
         setSupportActionBar(myToolbar);
         //todo delete once fragments are completely done
-        mTextMessage = (TextView) findViewById(R.id.message);
         //wires bottom navigation
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -68,19 +77,31 @@ public class HomePageActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int id = item.getItemId();
+
+            //Prepare a null fragment
+            Fragment currentFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
-                    //todo load search fragment
-                    return true;
+                    Log.d(TAG, "onNavigationItemSelected: search");
+                    currentFragment = new SearchFragment();
+                    break;
                 case R.id.navigation_favorites:
-                    mTextMessage.setText(R.string.title_favorites);
-                    //todo load favorites fragment
-                    return true;
+                    Log.d(TAG, "onNavigationItemSelected: favorites");
+                    currentFragment = new FavoritesFragment();
+                    break;
                 case R.id.navigation_create:
-                    mTextMessage.setText(R.string.title_create);
-                    //todo load create fragment
-                    return true;
+                    Log.d(TAG, "onNavigationItemSelected: create");
+                    currentFragment = new CreateFragment();
+                    break;
+            }
+            FragmentManager fm = getSupportFragmentManager();
+            if(currentFragment != null)
+            {
+                fm.beginTransaction()
+                        .replace(R.id.fragment_container, currentFragment)
+                        .commit();
+                return true;
             }
             return false;
         }
@@ -103,5 +124,5 @@ public class HomePageActivity extends AppCompatActivity {
         }
     }
 
-
+//
 }
--- app/src/main/java/com/example/caroline/foodme/LoginScreen.java
+++ app/src/main/java/com/example/caroline/foodme/LoginScreen.java
@@ -1,11 +1,9 @@
 package com.example.caroline.foodme;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -19,23 +17,26 @@ public class LoginScreen extends AppCompatActivity {
     private Button login, newAccount, help;
     private EditText usernameInput, passwordInput;
     private CheckBox rememberMe;
-    private TextView username, password;
-    private Toolbar toolbar;
+    private TextView foodMe, username, password;
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        //todo save UserID from bsckendless
+        /* editor = sharedPref.edit();
+        editor.putString(getString(R.string.user_ID), "");
+        editor.putBoolean(getString(R.string.user), true); //means there is a saved user
+        editor.commit(); */
 
         wireWidgets();
         //TODO wire login button to HomePageActivity, newAccount to CreateAccount, set onClickListeners for all
         //TODO link to Backendless
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.login_action_bar, menu);
-        return true;
     }
 
     public void wireWidgets(){
@@ -45,27 +46,8 @@ public class LoginScreen extends AppCompatActivity {
         usernameInput = (EditText) findViewById(R.id.username_editText);
         passwordInput = (EditText) findViewById(R.id.password_editText);
         rememberMe = (CheckBox) findViewById(R.id.remember_me_checkBox);
+        foodMe = (TextView) findViewById(R.id.food_me_textView);
         username = (TextView) findViewById(R.id.username_textView);
         password = (TextView) findViewById(R.id.password_textView);
-        toolbar= (Toolbar)findViewById(R.id.toolbar_login);
-        setSupportActionBar(toolbar);
-
-
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_help:
-                //enters settings activity
-                //todo  intetn for help
-                return true;
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
     }
 }
