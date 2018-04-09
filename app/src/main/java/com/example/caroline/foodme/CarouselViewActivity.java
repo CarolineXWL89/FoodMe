46cbfd2daffc32a8a8b2bcf032ee991958519891
michaelxiong <michaelxiong2001Gmail.com>
2018-04-09 12:33:50 -0700

    Michael 7: CarouselView updated

--- app/src/main/java/com/example/caroline/foodme/CarouselViewActivity.java
+++ app/src/main/java/com/example/caroline/foodme/CarouselViewActivity.java
@@ -2,11 +2,13 @@ package com.example.caroline.foodme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
+import com.synnapps.carouselview.ViewListener;
 
 import java.util.ArrayList;
 
@@ -25,7 +27,7 @@ public class CarouselViewActivity extends AppCompatActivity {
 
         carouselView = (CarouselView) findViewById(R.id.carouselView);
         carouselView.setPageCount(images.size());
-        carouselView.setImageListener(imageListener);
+        carouselView.setViewListener(viewListener);
     }
 
     //Add images to the arraylist here, these images will scroll
@@ -33,10 +35,16 @@ public class CarouselViewActivity extends AppCompatActivity {
         images = new ArrayList<Integer>();
     }
 
-    ImageListener imageListener = new ImageListener() {
+    ViewListener viewListener = new ViewListener() {
+
         @Override
-        public void setImageForPosition(int position, ImageView imageView) {
+        public View setViewForPosition(int position) {
+            View customView = getLayoutInflater().inflate(R.layout.carousel_item, null);
+            ImageView imageView = customView.findViewById(R.id.carousel_image_view);
+            TextView textView = customView.findViewById(R.id.carousel_text_view);
             imageView.setImageResource(images.get(position));
+            textView.setText("Title");
+            return customView;
         }
     };
 
