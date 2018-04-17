0386f5cb8ede1116e11d57338140163c659ca435
CarolineXWL89 <carolinexwliu89@hotmail.com>
2018-04-17 13:36:41 -0700

    Caroline #8 - Call for Nutrition

---
+++ app/src/main/java/com/example/caroline/foodme/EntitySearch.java
@@ -0,0 +1,9 @@
+package com.example.caroline.foodme;
+
+/**
+ * Created by princ on 17/04/2018.
+ * To get the information to plug into nutrition search
+ */
+
+public class EntitySearch {
+}
--- app/src/main/java/com/example/caroline/foodme/ParserAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.example.caroline.foodme;
-
-/**
- * Created by princ on 09/04/2018.
- */
-
-public class ParserAdapter {
-}
