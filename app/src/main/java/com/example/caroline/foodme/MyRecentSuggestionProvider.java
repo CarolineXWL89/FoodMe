bc52f19885e4fdb3bc24dccec350770403d95c94
Nicolo <nicolo.porcu@gmail.com>
2018-04-19 21:32:37 -0700

    h

---
+++ app/src/main/java/com/example/caroline/foodme/MyRecentSuggestionProvider.java
@@ -0,0 +1,15 @@
+package com.example.caroline.foodme;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+/**
+ * Created by Nicolo on 4/19/2018.
+ */
+public class MyRecentSuggestionProvider extends SearchRecentSuggestionsProvider {
+    public final static String AUTHORITY = "com.example.MyRecentSuggestionProvider";
+    public final static int MODE = DATABASE_MODE_QUERIES;
+
+    public MyRecentSuggestionProvider() {
+        setupSuggestions(AUTHORITY, MODE);
+    }
+}
