61da91e2004e825224cbaf7e3fa8a7ea31a99c8e
CarolineXWL89 <carolinexwliu89@hotmail.com>
2018-04-27 14:02:09 -0700

    Caroline #11 - Database part 4/a million

--- app/src/main/java/com/example/caroline/foodme/CreateFragment.java
+++ app/src/main/java/com/example/caroline/foodme/CreateFragment.java
@@ -7,6 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class CreateFragment extends Fragment {
 
     public static final String TAG = "fragments";
@@ -23,6 +31,34 @@ public class CreateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        final ArrayList<EntitySearch> entitySearches = new ArrayList<>();
+        String foodSearched = "";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(DataMuseNutritionIngr.baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        DataMuseNutritionIngr api = retrofit.create(DataMuseNutritionIngr.class);
+
+
+        Call<ArrayList<EntitySearch>> call = api.getIngrNutrient(foodSearched, EdamamKeys.APP_ID, EdamamKeys.APP_KEY);
+
+        call.enqueue(new Callback<ArrayList<EntitySearch>>() {
+            @Override
+            public void onResponse(Call<ArrayList<EntitySearch>> call, Response<ArrayList<EntitySearch>> response) {
+                entitySearches.clear();
+                entitySearches.addAll(response.body());
+            }
+            @Override
+            public void onFailure(Call<ArrayList<EntitySearch>> call, Throwable t) {
+                //NOTHING YET
+            }
+        });
+
+        final food.json 
+
         return inflater.inflate(R.layout.fragment_create, container, false);
     }
 
