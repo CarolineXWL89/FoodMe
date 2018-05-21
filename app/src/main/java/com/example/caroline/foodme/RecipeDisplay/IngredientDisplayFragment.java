ef767b0ac6722ca0199e56d1eedbceac88b73fc4
per6 <sw498480689>
2018-05-21 13:54:38 -0700

    did something

--- app/src/main/java/com/example/caroline/foodme/RecipeDisplay/IngredientDisplayFragment.java
+++ app/src/main/java/com/example/caroline/foodme/RecipeDisplay/IngredientDisplayFragment.java
@@ -92,7 +92,7 @@ public class IngredientDisplayFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
-
+//
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
